eab04020a50ee9bec1c5afd5e0ae11142063cf6e
Michael Remington <michaelremington2@gmail.com>
2024-07-17 14:46:03 -0700

    Made agent based classes.

---
+++ src/therma_sim/agents.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import mesa
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import networkx as nx
+import pandas as pd
+
+class Kangaroo_Rat(mesa.Agent):
+    '''
+    Agent Class for kangaroo rat agents.
+      A kangaroo rat agent is one that is at the bottom of the trophic level and only gains energy through foraging from the 
+    seed patch class.
+    '''
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
+class Rattlesnake(mesa.Agent):
+    '''
+    Agent Class for rattlesnake predator agents.
+        Rattlsnakes are sit and wait predators that forage on kangaroo rat agents
+    '''
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
---
+++ src/therma_sim/landscape.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import mesa
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import networkx as nx
+import pandas as pd
