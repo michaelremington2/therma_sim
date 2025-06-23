35b607056b2f7e8a205bd411f060642771920c97
Michael <michaelremington2@gmail.com>
2025-06-23 13:50:49 -0700

    Made it so individual data is only collected during active hours.

--- src/therma_sim/model.py
+++ src/therma_sim/model.py
@@ -516,8 +516,10 @@ class ThermaSim(mesa.Model):
         # Snakes
         snake_shuffle = self.randomize_snakes()
         for snake in snake_shuffle:
-            data = snake.report_data()
-            self.logger.log_data(file_name = self.output_folder+"Rattlesnake.csv", data=data)
+            # Only collect data if the snake is active
+            if self.hour in snake.active_hours and not snake.is_bruminating_today():
+                data = snake.report_data()
+                self.logger.log_data(file_name = self.output_folder+"Rattlesnake.csv", data=data)
             snake.step()
             data = None
         snake_shuffle = self.randomize_snakes()
